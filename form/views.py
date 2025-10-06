031e5f4a3eb0c1cbdb4033d7475fb90d56523d44
Draminhon <britomurilorog@gmail.com>
2025-10-06 12:30:25 -0300

    adicionando autenticação por token

--- form/views.py
+++ form/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_api_key.permissions import HasAPIKey
 from .models import *
 from .serializers import * 
 
@@ -9,31 +10,26 @@ class TuristaViewSet(viewsets.ModelViewSet):
     queryset = Turista.objects.all()
     serializer_class = TuristaSerializer
 
-    permission_classes = [permissions.AllowAny]
 
 class ViagemViewSet(viewsets.ModelViewSet):
     queryset = Viagem.objects.all()
     serializer_class = ViagemSerializer
 
-    permission_classes = [permissions.AllowAny]
 
 class AvaliacaoViewSet(viewsets.ModelViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
-    permission_classes = [permissions.AllowAny]
 
 class AtividadesRealizadasViewSet(viewsets.ModelViewSet):
     queryset = AtividadesRealizadas.objects.all()
     serializer_class = AtividadesRealizadasSerializer
 
-    permission_classes = [permissions.AllowAny]
 
 class PlanejamentoViewSet(viewsets.ModelViewSet):
     queryset = Planejamento.objects.all()
     serializer_class = PlanejamentoSerializer
 
-    permission_classes = [permissions.AllowAny]
 
 
 
